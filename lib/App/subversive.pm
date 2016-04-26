79227c3c15e8b30d584cf435d148d4b344d39851
Graham Ollis <plicease@cpan.org>
2016-04-26 18:04:08 -0400

    more oops

--- lib/App/subversive.pm
+++ lib/App/subversive.pm
@@ -119,22 +119,18 @@ sub update
     
     foreach my $file (@git_files)
     {
-      $DB::single = 1;
       my $git_file = $root->subdir('git')->file($file);
       $file->parent->mkpath(0, 0700) unless -d $file->parent;
       cp($git_file => $file) || die "Copy failed for $git_file => $file: $!";
       svn('add', '--force', '--parents', $file);
       
-      if(-x $file && !$exe{"$file"})
+      if(-x $file && !$exe{$file->relative})
       {
         svn('propset', 'svn:executable', 'on', $file);
       }
-      if((! -x $file) && $exe{"$file"})
+      if(! -x $file && $exe{$file->relative})
       {
-        if($exe{"$file"})
-        {
-          svn('propdel', 'svn:executable', $file);
-        }
+        svn('propdel', 'svn:executable', $file);
       }
     }
     
